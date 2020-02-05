4a24f09ef91f3a7efead3196c1402c10c6014250
Alejandro Alcalá Álvarez <alejandro.alcala@estudiante.uam.es>
2020-02-05 10:37:12 +0100

    Comentado el código y formateado.

--- P0/clienteEco.py
+++ P0/clienteEco.py
@@ -1,51 +1,43 @@
-# -*- coding: windows-1252 -*-
+# -*- coding: utf-8 -*-
 import socket
 import time
 import struct
-# Cabecera RTP
-# P = 0
-# X = 0
-# Version 2
-# timestamp = entero de 32 bits del time.time()
-# Identificador unico para toda la conexion
-# Debajo todo la informacion
 
 # Declaracion de variables
 IP = "127.0.0.1"
 PORT = 5004
+# Número de secuencia de la cabecera RTP
 SEQ = 0
+# Número de Identificador de fuente de sincronización de la cabecera RTP
 SSRC_id = 1
-print ("Direccion IP de destino:", IP)
-print ("Puerto UDP de destino:", PORT)
 
-
-# Se crea un socket
+# Creamos el socket
 sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
 
-# Se coge el tiempo antes de enviar
+# Bucle para poder enviar todos los mensajes que queramos
 while(1):
+    # Cogemos el tiempo antes de enviar el mensaje
     tenvio=time.time()
+    # Obtenemos por teclado el mensaje que queremos enviar
     entrada = input("Introduce el mensaje: ")
     MESSAGE = str.encode(entrada) # Los mensajes son arrays de bytes, no strings
 
     # Construccion cabecera RTP
     cabecera = struct.pack("!HHII", 0x8014,SEQ, int(tenvio), SSRC_id)
+    # Construcción del mensaje que enviamos al servidor
     data_envio = cabecera +MESSAGE
-    print(data_envio)
     # Se envia el mensaje al destino
     sock.sendto(data_envio, (IP, PORT))
+    # Aumentamos el número de secuencia
     SEQ += 1
-
     # Se espera la respuesta
     data, addr = sock.recvfrom(2048)
     # Se calcula el RTT
     rtt = time.time()-tenvio
-
     # Se comprueba la respuesta
     if data==data_envio:
-    	print ("Mensaje devuelto correctamente")
-    	print ("RTT", rtt)
+    	print ("Tiem de respuesta:" +str(rtt)+" milisegundos")
     else:
     	print ("No volvio el mensaje correctamente")
 
--- P0/servidorEco.py
+++ P0/servidorEco.py
@@ -1,9 +1,9 @@
-# -*- coding: windows-1252 -*-
+# -*- coding: utf-8 -*-
 import socket
 import time
 import struct
 
-# Declaraci�n de variables
+# Declaracion de variables
 IP_SERVER = "127.0.0.1"
 PORT_SERVER = 5004
 
@@ -15,10 +15,14 @@ sock.bind((IP_SERVER, PORT_SERVER))
 
 while True:
 	# Se pone el servidor a la escucha
-    data, addr = sock.recvfrom(2048) # Tamaño del buffer de recepci�n
+    data, addr = sock.recvfrom(2048) # Tamaño del buffer de recepcion
+    # Obtenemos la cabecera RTP del mensaje 
     cabecera = struct.unpack('!HHII',data[0:12])
-    print(cabecera[1])
+    # Obtenemos el mensaje que ha enviado el cliente
     msg = data[12:]
-    print(msg.decode())
-    #print(data.decode())
+    # Mostramos el mensaje
+    print(">>>"+str(msg.decode()))
+    # Enviamos la cabecera RTP para dar confirmacion de que se ha recibido el mensaje
     sock.sendto(data, addr)
+
+print("Se finalizo la conexión ...")
