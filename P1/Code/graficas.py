a679bd96fa01c12231ee2070efcb2909ebc0b7c2
Pablo Diez <pablo.diezpozo@gmail.com>
2020-02-23 23:38:08 +0100

    Nuevas capturas de trafico

--- P1/Code/graficas.py
+++ P1/Code/graficas.py
@@ -3,15 +3,10 @@ import pandas as pd
 from collections import Counter
 import numpy as np
 
-df = pd.read_csv("../Doc/Emulador3_TiemposPuerto5005")
+df = pd.read_csv("../Resultados/em2_5005")
 
-tiempos = df['Time']
+tiempos = df.Time
 
-labels, values = zip(*Counter(tiempos).items())
-
-indexes = np.arange(len(labels))
-width = 1
-
-plt.bar(indexes,values,width)
-plt.xticks(indexes + width * 0.5, labels)
+plt.hist(tiempos, bins= 'auto')
 plt.show()
+s
