47895033257ab8ecf1da401cd9831c5aa54b8ef0
Alejandro Alcalá Álvarez <alejandro.alcala@estudiante.uam.es>
2020-02-28 10:20:45 +0100

    Solucionando el tema de los tiempos.

--- P2/Code/codigoTrenPaquetes/servidorTren.py
+++ P2/Code/codigoTrenPaquetes/servidorTren.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 		#ATENCIÓN: El tiempo de recepción está en formato: segundos.microsegundos
 		#Usar este tiempo para calcular los anchos de banda
 		reception_time=packet[1] #Para calcula el ancho de banda
-		tamanoPackets = len(data[12:])
+		tamanoPackets = len(data[12:])*8
 		npackets+=1
 		#Truncamos el tiempo de recepción a centésimas de milisegundos 
 		#(o decenas de microsegundos, segun se quiera ver) y 32 bits
@@ -76,13 +76,12 @@ if __name__ == "__main__":
 
 		print ('Retardo instantaneo en un sentido (s): ',(reception_time_trunc-send_time_trunc)/DECENASMICROSECS)
 		retardos.append((reception_time_trunc-send_time_trunc)/DECENASMICROSECS)
-		if len(packet_list) - 1:
-			if npackets < 10:
-				reception_time_trunc_i1 = int(packet_list[npackets][1] * DECENASMICROSECS)&B_MASK
-			if (reception_time_trunc_i1 - reception_time_trunc) == 0:
+		if npackets > 1:
+			reception_time_i1 = packet_list[npackets-2][1]
+			if (reception_time_i1 - reception_time) == 0:
 				anchoBandaInstantaneo = len(data[12:])
 			else:
-				anchoBandaInstantaneo = len(data[12:]) / ((reception_time_trunc_i1 - reception_time_trunc)/DECENASMICROSECS)
+				anchoBandaInstantaneo = len(data[12:]) / (reception_time - reception_time_i1)
 			anchosBandas.append(anchoBandaInstantaneo)
 			print ('Ancho de Banda Instantáneo (b/s): ', anchoBandaInstantaneo)
 		###########################PRÁCTICA##############################################
@@ -93,15 +92,14 @@ if __name__ == "__main__":
 		#         (o decenas de microsegundos, segun se quiera ver)                     #
 		# a la hora de calcular retardos se debe tener en cuenta                        #
 		#################################################################################
-	reception_time_trunc_1 = int(packet_list[0][1] * DECENASMICROSECS)&B_MASK  
-	reception_time_trunc_N = int(packet_list[len(packet_list)-1][1] * DECENASMICROSECS)&B_MASK
-	anchoBandaMedio = ((npackets - 1) * tamanoPackets)/ ((reception_time_trunc_N - reception_time_trunc_1)/DECENASMICROSECS)
+	reception_time_1 = packet_list[0][1]
+	reception_time_N = packet_list[-1][1]
+	anchoBandaMedio = ((npackets - 1) * tamanoPackets)/ (reception_time_N - reception_time_1)
 	print ('Ancho de Bando Medio (b/s): ', anchoBandaMedio)
 	print ('Ancho de Banda Medio (Mb/s): ', (anchoBandaMedio/1000))
 	print ('Ancho de Banda Medio (Gb/s): ', (anchoBandaMedio/1000000))
-	anchosBandas.sort(reverse=False)
-	print ('Ancho de Banda Instantáneo Máximo (b/s): ', anchosBandas[0])
-	print ('Ancho de Banda Instantáneo Mínimo (b/s): ', anchosBandas[-1])
+	print ('Ancho de Banda Instantáneo Máximo (b/s): ', max(anchosBandas))
+	print ('Ancho de Banda Instantáneo Mínimo (b/s): ', min(anchosBandas))
 	###########################PRÁCTICA##############################################
 	#                                                                               #
 	# Añadir cálculos necesarios para obtener pérdida de paquetes y variación del   #
@@ -111,13 +109,12 @@ if __name__ == "__main__":
 	print ('Número de paquetes recibidos: ', npackets)
 	print ('Perdida de paquetes: ',packetLoss)
 	print ('Porcentaje de Perdida de paquetes: ', (packetLoss*100)/npackets)
-	retardos.sort(reverse=True)
 	jitter=0
 	sumaRetardo = 0
 	for ret in retardos:
 		sumaRetardo += ret
-	print ('Retardo Máximo en (s): ', retardos[0])
-	print ('Retardo Mínimo en (s): ', retardos[-1])
+	print ('Retardo Máximo en (s): ', max(retardos))
+	print ('Retardo Mínimo en (s): ', min(retardos))
 	print ('Retardo Medio en (s): ', (sumaRetardo/len(retardos)))
 	print ('Variación del retardo: ',jitter)
 	#################################################################################
